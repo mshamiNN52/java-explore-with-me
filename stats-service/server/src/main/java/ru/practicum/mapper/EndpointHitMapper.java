4a3ebd6511a4ecb660f6f17492719c1925030faa
mshamiNN52 <124524683+mshamiNN52@users.noreply.github.com>
2023-10-17 19:36:40 +0300

    Add files via upload

---
+++ stats-service/server/src/main/java/ru/practicum/mapper/EndpointHitMapper.java
@@ -0,0 +1,31 @@
+package ru.practicum.mapper;
+
+import lombok.experimental.UtilityClass;
+import ru.practicum.dto.EndpointHitDto;
+import ru.practicum.model.EndpointHit;
+
+import java.time.LocalDateTime;
+
+@UtilityClass
+public class EndpointHitMapper {
+
+    public static EndpointHit toEntity(EndpointHitDto dto) {
+        return EndpointHit.builder()
+                .app(dto.getApp())
+                .ip(dto.getIp())
+                .uri(dto.getUri())
+                .timestamp(LocalDateTime.parse(dto.getTimestamp()))
+                .build();
+
+    }
+
+    public static EndpointHitDto toDto(EndpointHit entity) {
+        return EndpointHitDto.builder()
+                .id(entity.getId())
+                .app(entity.getApp())
+                .ip(entity.getIp())
+                .uri(entity.getUri())
+                .timestamp(String.valueOf(entity.getTimestamp()))
+                .build();
+    }
+}
