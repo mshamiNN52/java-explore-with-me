02475fd7d3f3e5527b44a86221171855d2b4454a
mshamiNN52 <124524683+mshamiNN52@users.noreply.github.com>
2023-10-19 20:48:46 +0300

    2

--- main-service/src/main/java/ru/practicum/mainservice/service/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/mainservice/service/EventServiceImpl.java
@@ -115,8 +115,9 @@ public class EventServiceImpl implements EventService {
                 }
                 event.setState(newState);
             }
+        }
             event = EventMapper.INSTANCE.forUpdate(updater, newCategory, newState, event);
-        } return EventMapper.INSTANCE.toDto(event, 0L, 0L);
+        return EventMapper.INSTANCE.toDto(event, 0L, 0L);
     }
 
     @Transactional
