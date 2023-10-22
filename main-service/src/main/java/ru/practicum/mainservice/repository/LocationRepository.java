782a836d922e8e6516250ea8d48fa31487c4b143
mshamiNN52 <124524683+mshamiNN52@users.noreply.github.com>
2023-10-22 16:34:55 +0300

    Delete main-service directory

--- main-service/src/main/java/ru/practicum/mainservice/repository/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.mainservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.practicum.mainservice.model.Location;
-
-@Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
